e7971cf0a6a20cd9ea444eb91b440190c24086e7
Francisco <fmartinezv2002@gmail.com>
2024-07-22 13:15:56 -0400

    Implement number buttons functionality and display

--- app.js
+++ app.js
@@ -30,12 +30,24 @@ function operate(a, b, operator) {
 }
 
 // Variables for each part of a calculator operation
-let a;
-let b;
+let a = "";
+let b = "";
 let operator;
 
 // Variables to store HTML elements
-const display = document.querySelector("display");
-const buttonsContainer = document.querySelector("buttons-container");
-const numberButtons = buttonsContainer.querySelectorAll("number-buttons-container"); 
+const display = document.querySelector("#display");
+const buttonsContainer = document.querySelector("#buttons-container");
+const numberButtons = buttonsContainer.querySelectorAll("#number-buttons-container"); 
+
+// Add event listener to each number button
+numberButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        if (display.innerText == "Waiting for numbers") {
+            display.innerText = "";
+        }
+    
+        a += event.target.innerText;
+        display.innerText = a;
+    });
+});
 
