b7a426f74efb26bab037cc529de31af975b0f81f
Francisco <fmartinezv2002@gmail.com>
2024-07-27 08:20:01 -0400

    Add division by 0 functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
@@ -16,16 +16,16 @@ function divide(a, b) {
 
 function operate(a, b, operator) {
     if (operator == "+") {
-        add(a, b);
+        expression = add(a, b);
     }
     else if (operator == "-"){
-        subtract(a, b);
+        expression = subtract(a, b);
     }
     else if (operator == "*"){
-        multiply(a, b);
+        expression = multiply(a, b);
     }
     else if (operator == "/"){
-        divide(a, b);
+            expression = divide(a, b);
     }
 }
 
@@ -34,13 +34,13 @@ function reset() {
     a = "";
     b = "";
     operator = "";
-    display.innerText = "Waiting for numbers";
 }
 
 // Variables for each part of a calculator operation
 let a = "";
 let b = "";
 let operator = "";
+let expression = "Waiting for numbers";
 
 // Variables to store HTML elements
 const display = document.querySelector("#display");
@@ -48,17 +48,53 @@ const buttonsContainer = document.querySelector("#buttons-container");
 
 // Buttons container event listener
 buttonsContainer.addEventListener("click", event => {
-    // Check if the clicked element is a number button within 
-    // the number-buttons-container
+    // Check if the clicked element is a number button 
     if (event.target.tagName === "BUTTON" && 
         event.target.parentElement.id === "number-buttons-container") {
-        if (display.innerText == "Waiting for numbers") {
-            display.innerText = "";
+        // If it is the first number
+        if (operator == "") {
+            a += event.target.innerText;
         }
-
-        a += event.target.innerText;
-        display.innerText = a;
+        // If it is the second number
+        else {
+            b += event.target.innerText;
+        }
+        expression = a + " " + operator + " " + b;
     }
+
+    // Check if the clicked button is an operation button 
+    if (event.target.tagName === "BUTTON" &&
+        event.target.parentElement.id == "operation-buttons-container") {
+        operator = event.target.innerText;
+
+        expression = a + " " + operator + " " + b;
+    }
+
+    // Check if the clicked button is a functionality button
+    if (event.target.tagName === "BUTTON" &&
+        event.target.parentElement.id === "functionality-buttons-container") {
+        // If we have to delete the current expression
+        if (event.target.innerText == "A/C") {
+            reset();
+            expression = "Waiting for numbers";
+        }
+        // If we have to calculate the current expression
+        if (event.target.innerText == "=") {
+            if (b != "/" && b != "0") {
+                operate(a, b, operator);
+                a = expression;
+                b = "";
+                operator = "";
+            }
+            else {
+                window.alert("Are you seriously trying to divide by 0? How dumb");
+                reset();
+                expression = "Waiting for numbers";
+            }
+        }
+    }
+    
+    display.innerText = expression;
 });
 
 
